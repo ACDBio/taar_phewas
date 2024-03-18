753b9b8d2ed36578a8d28c56f6d4da4cc3acbe7f
acdbio <ptherid@gmail.com>
2024-03-19 00:01:41 +0300

    Added a table with trait ranking

--- app.py
+++ app.py
@@ -67,6 +67,10 @@ def update_bar_plot(n_clicks, rows, derived_virtual_selected_rows):
 
     counts=pd.DataFrame(dff['subcategory'].value_counts()).reset_index().rename(columns={'index':'subcategory','subcategory':'association_count'})
     counts_genes=pd.DataFrame(dff['gs'].value_counts()).reset_index().rename(columns={'index':'gene','gs':'association_count'})
+    min_p_indices = dff.groupby('trait')['p'].idxmin()
+    filtered_data = dff.loc[min_p_indices]
+    filtered_data = dff[['trait','is_gene_trait','p','beta','rsid', 'gs']]
+    filtered_data=filtered_data.sort_values('p')
     # Create a bar plot
     return [
         dcc.Graph(
@@ -112,7 +116,25 @@ def update_bar_plot(n_clicks, rows, derived_virtual_selected_rows):
                     "margin": {"t": 20, "l": 10, "r": 10},
                 },
             },
-        ),        
+        ),
+        dcc.Markdown('Traits arranged by max association strength'),
+        dash_table.DataTable(
+            id='trait_ranking_table',
+            columns=[{"name": i, "id": i, "type": "numeric" if i in ['p', 'beta','se','z','sample_size'] else "text"} for i in filtered_data.columns],
+            data=filtered_data.to_dict('records'),
+            editable=True,
+            filter_action="native", # Enable filtering
+            sort_action="native", # Enable sorting
+            sort_mode="multi", # Allow sorting by multiple columns
+            column_selectable="single", # Allow selecting columns
+        # row_selectable="multi", # Allow selecting multiple rows
+        # row_deletable=True, # Allow deleting rows
+            selected_columns=[], # Initially selected columns
+        #  selected_rows=[], # Initially selected rows
+            page_action="native", # Enable pagination
+            page_current= 0, # Initial page
+            page_size= 15, # Rows per page
+        )        
         ]
 
 @app.callback(
